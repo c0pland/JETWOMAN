f01037430ba7c132b47bffe06545fc8f7c61696c
Bogdan <c0pland@icloud.com>
2022-08-09 16:19:26 +0300

    Initial Commit

---
+++ JETWOMAN/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  JETWOMAN
+//
+//  Created by Богдан Беннер on 09.08.2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
